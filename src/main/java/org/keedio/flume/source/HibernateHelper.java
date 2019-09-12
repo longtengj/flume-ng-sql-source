c858c16127fb5050a981a9f38262380eb4937006
longtengjun <longtengjun@xinchengbio.com>
2019-09-12 10:44:22 +0800

    reseting connection should check if the session is open or not

--- src/main/java/org/keedio/flume/source/HibernateHelper.java
+++ src/main/java/org/keedio/flume/source/HibernateHelper.java
@@ -19,133 +19,140 @@ import org.apache.flume.Context;
 
 /**
  * Helper class to manage hibernate sessions and perform queries
- * 
- * @author <a href="mailto:mvalle@keedio.com">Marcelo Valle</a>
  *
+ * @author <a href="mailto:mvalle@keedio.com">Marcelo Valle</a>
  */
 public class HibernateHelper {
 
-	private static final Logger LOG = LoggerFactory
-			.getLogger(HibernateHelper.class);
+    private static final Logger LOG = LoggerFactory
+            .getLogger(HibernateHelper.class);
 
-	private static SessionFactory factory;
-	private Session session;
-	private ServiceRegistry serviceRegistry;
-	private Configuration config;
-	private SQLSourceHelper sqlSourceHelper;
+    private static SessionFactory factory;
+    private Session session;
+    private ServiceRegistry serviceRegistry;
+    private Configuration config;
+    private SQLSourceHelper sqlSourceHelper;
 
-	/**
-	 * Constructor to initialize hibernate configuration parameters
-	 * @param sqlSourceHelper Contains the configuration parameters from flume config file
-	 */
-	public HibernateHelper(SQLSourceHelper sqlSourceHelper) {
+    /**
+     * Constructor to initialize hibernate configuration parameters
+     *
+     * @param sqlSourceHelper Contains the configuration parameters from flume config file
+     */
+    public HibernateHelper(SQLSourceHelper sqlSourceHelper) {
 
-		this.sqlSourceHelper = sqlSourceHelper;
-		Context context = sqlSourceHelper.getContext();
+        this.sqlSourceHelper = sqlSourceHelper;
+        Context context = sqlSourceHelper.getContext();
 
-		/* check for mandatory propertis */
-		sqlSourceHelper.checkMandatoryProperties();
+        /* check for mandatory propertis */
+        sqlSourceHelper.checkMandatoryProperties();
 
-		Map<String,String> hibernateProperties = context.getSubProperties("hibernate.");
-		Iterator<Map.Entry<String,String>> it = hibernateProperties.entrySet().iterator();
-		
-		config = new Configuration();
-		Map.Entry<String, String> e;
-		
-		while (it.hasNext()){
-			e = it.next();
-			config.setProperty("hibernate." + e.getKey(), e.getValue());
-		}
+        Map<String, String> hibernateProperties = context.getSubProperties("hibernate.");
+        Iterator<Map.Entry<String, String>> it = hibernateProperties.entrySet().iterator();
 
-	}
+        config = new Configuration();
+        Map.Entry<String, String> e;
 
-	/**
-	 * Connect to database using hibernate
-	 */
-	public void establishSession() {
+        while (it.hasNext()) {
+            e = it.next();
+            config.setProperty("hibernate." + e.getKey(), e.getValue());
+        }
 
-		LOG.info("Opening hibernate session");
+    }
 
-		serviceRegistry = new StandardServiceRegistryBuilder()
-				.applySettings(config.getProperties()).build();
-		factory = config.buildSessionFactory(serviceRegistry);
-		session = factory.openSession();
-		session.setCacheMode(CacheMode.IGNORE);
-		
-		session.setDefaultReadOnly(sqlSourceHelper.isReadOnlySession());
-	}
+    /**
+     * Connect to database using hibernate
+     */
+    public void establishSession() {
 
-	/**
-	 * Close database connection
-	 */
-	public void closeSession() {
+        LOG.info("Opening hibernate session");
 
-		LOG.info("Closing hibernate session");
+        serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(config.getProperties()).build();
+        factory = config.buildSessionFactory(serviceRegistry);
+        session = factory.openSession();
+        session.setCacheMode(CacheMode.IGNORE);
 
-		session.close();
-		factory.close();
-	}
+        session.setDefaultReadOnly(sqlSourceHelper.isReadOnlySession());
+    }
 
-	/**
-	 * Execute the selection query in the database
-	 * @return The query result. Each Object is a cell content. <p>
-	 * The cell contents use database types (date,int,string...), 
-	 * keep in mind in case of future conversions/castings.
-	 * @throws InterruptedException 
-	 */
-	@SuppressWarnings("unchecked")
-	public List<List<Object>> executeQuery() throws InterruptedException {
-		
-		List<List<Object>> rowsList = new ArrayList<List<Object>>() ;
-		Query query;
-		
-		if (!session.isConnected()){
-			resetConnection();
-		}
-				
-		if (sqlSourceHelper.isCustomQuerySet()){
-			
-			query = session.createSQLQuery(sqlSourceHelper.buildQuery());
-			
-			if (sqlSourceHelper.getMaxRows() != 0){
-				query = query.setMaxResults(sqlSourceHelper.getMaxRows());
-			}			
-		}
-		else
-		{
-			query = session
-					.createSQLQuery(sqlSourceHelper.getQuery())
-					.setFirstResult(Integer.parseInt(sqlSourceHelper.getCurrentIndex()));
-			
-			if (sqlSourceHelper.getMaxRows() != 0){
-				query = query.setMaxResults(sqlSourceHelper.getMaxRows());
-			}
-		}
-		
-		try {
-			rowsList = query.setFetchSize(sqlSourceHelper.getMaxRows()).setResultTransformer(Transformers.TO_LIST).list();
-		}catch (Exception e){
-			LOG.error("Exception thrown, resetting connection.",e);
-			resetConnection();
-		}
-		
-		if (!rowsList.isEmpty()){
-			if (sqlSourceHelper.isCustomQuerySet()){
-					sqlSourceHelper.setCurrentIndex(rowsList.get(rowsList.size()-1).get(0).toString());
-			}
-			else
-			{
-				sqlSourceHelper.setCurrentIndex(Integer.toString((Integer.parseInt(sqlSourceHelper.getCurrentIndex())
-						+ rowsList.size())));
-			}
-		}
-		
-		return rowsList;
-	}
+    /**
+     * Close database connection
+     */
+    public void closeSession() {
 
-	private void resetConnection() throws InterruptedException{
-		session.close();
-		factory.close();
-		establishSession();
-	}
+        LOG.info("Closing hibernate session");
+
+        session.close();
+        factory.close();
+    }
+
+    /**
+     * Execute the selection query in the database
+     *
+     * @return The query result. Each Object is a cell content. <p>
+     * The cell contents use database types (date,int,string...),
+     * keep in mind in case of future conversions/castings.
+     * @throws InterruptedException
+     */
+    @SuppressWarnings("unchecked")
+    public List<List<Object>> executeQuery() throws InterruptedException {
+
+        List<List<Object>> rowsList = new ArrayList<List<Object>>();
+        Query query;
+
+        if (!session.isConnected()) {
+            resetConnection();
+        }
+
+        if (sqlSourceHelper.isCustomQuerySet()) {
+
+            query = session.createSQLQuery(sqlSourceHelper.buildQuery());
+
+            if (sqlSourceHelper.getMaxRows() != 0) {
+                query = query.setMaxResults(sqlSourceHelper.getMaxRows());
+            }
+        } else {
+            query = session
+                    .createSQLQuery(sqlSourceHelper.getQuery())
+                    .setFirstResult(Integer.parseInt(sqlSourceHelper.getCurrentIndex()));
+
+            if (sqlSourceHelper.getMaxRows() != 0) {
+                query = query.setMaxResults(sqlSourceHelper.getMaxRows());
+            }
+        }
+
+        try {
+            rowsList = query.setFetchSize(sqlSourceHelper.getMaxRows()).setResultTransformer(Transformers.TO_LIST).list();
+        } catch (Exception e) {
+            LOG.error("Exception thrown, resetting connection.", e);
+            resetConnection();
+        }
+
+        if (!rowsList.isEmpty() && rowsList.size() > 0) {
+            if (sqlSourceHelper.isCustomQuerySet()) {
+                String cuentIndex = rowsList.get(rowsList.size() - 1).get(0).toString();
+                if (cuentIndex != null) {
+                    sqlSourceHelper.setCurrentIndex(cuentIndex);
+                }
+            } else {
+                sqlSourceHelper.setCurrentIndex(Integer.toString((Integer.parseInt(sqlSourceHelper.getCurrentIndex())
+                        + rowsList.size())));
+            }
+        }
+
+        return rowsList;
+    }
+
+    private void resetConnection() throws InterruptedException {
+        if (session.isOpen()) {
+
+            session.close();
+            factory.close();
+
+        } else {
+
+            establishSession();
+
+        }
+    }
 }
